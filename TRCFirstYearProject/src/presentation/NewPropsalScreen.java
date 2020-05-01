eac938a257ad3cdd39735e8cd48cc845349a9b63
Jamil <Jamil@Jamil-PC>
2020-05-01 17:42:23 +0200

    .

--- TRCFirstYearProject/src/presentation/NewPropsalScreen.java
+++ TRCFirstYearProject/src/presentation/NewPropsalScreen.java
@@ -8,7 +8,6 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
